ca7aecf9d9ce2ffe98de45168616964faeb29353
deibyssoca <deibyssoca@gmail.com>
2024-04-06 10:31:26 +0200

    new workflow terr

--- backend.tf
+++
@@ -1,9 +0,0 @@
-# backend.tf
-terraform {
-  backend "s3" {
-    # Replace this with your bucket name!
-    bucket = "ds-e-commerce-bucket"
-    key    = "go-lambda-test.tfstate"
-    region = "us-east-1"
-  }
-}
--- providers.tf
+++ providers.tf
@@ -1,9 +1,4 @@
 
 terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "5.7.0"
-    }
-  }
+  backend "s3" {}
 }
